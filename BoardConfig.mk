878809a201e22da997e0455540c075f49acc866f
More Robust Than You <MoreRobustThanYou@users.noreply.github.com>
2017-12-12 20:02:37 -0500

    twrp config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,11 +7,7 @@
 -include vendor/amazon/hdx-common/BoardConfigVendor.mk
 
 # Kernel
-ifeq ($(TARGET_BUILD_VARIANT),eng)
-TARGET_KERNEL_CONFIG := thor_eng_defconfig
-else
-TARGET_KERNEL_CONFIG := thor_defconfig
-endif
+TARGET_KERNEL_CONFIG := thor_twrp_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 
 # Assert
